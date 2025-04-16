c61f4496acc778283ec614cc55e234e9dd207605
kingsley <knwachukwu73@gmail.com>
2025-04-16 18:41:38 +0100

    my one commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,7 +26,7 @@ export default {
   data(){
     return {
     // baseURL: "https://kings1-web-e-commerce.onrender.com/api/v1/",
-     baseURL:"https://royalgoods.onrender.com/api/v1",
+     baseURL:"https://royalgoods.onrender.com/api/v1/",
       //baseURL: "http://localhost:3000/api/v1/",
       products:null,
       categories:null,
