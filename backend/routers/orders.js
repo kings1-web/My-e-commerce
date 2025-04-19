25d48c798a759ed4f9d67b9e3487f65e998dd85e
kingsley <knwachukwu73@gmail.com>
2025-04-19 06:43:29 +0100

    i wil surel

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -166,8 +166,8 @@ router.delete("/:id", (req, res) => {
     payment_method_types:['card'],
     line_items:lineItems,
     mode:'payment',
-    success_url:'https://royalgoods.onrender.com/payment/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url:`https://royalgoods.onrender.com/payment/failure`,
+    success_url:`https://kings-ecommerce.onrender.com/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url:`https://kings-ecommerce.onrender.com/payment/failure`,
     metadata:{
       billingAddress:JSON.stringify(billingAddress),
        items:JSON.stringify(items)
