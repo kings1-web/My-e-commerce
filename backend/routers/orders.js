71932c66276bef69d0ae54d5246550787048b3d4
kingsley <knwachukwu73@gmail.com>
2025-04-20 09:46:42 +0100

    your commit

--- backend/routers/orders.js
+++ backend/routers/orders.js
@@ -169,8 +169,8 @@ console.log('Address:',address)
     payment_method_types:['card'],
     line_items:lineItems,
     mode:'payment',
-    success_url:'https://kings-ecommerce.onrender.com/payment/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url:'https://kings-ecommerce.onrender.com/payment/failure',
+    success_url:'https://royalgoods.onrender.com/payment/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url:'https://royalgoods.onrender.com/payment/failure',
      metadata:{
       items:JSON.stringify(items),
       address:JSON.stringify(address)
