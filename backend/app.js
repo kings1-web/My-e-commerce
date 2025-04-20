bbf242fce84b21d31a79ffc9cbf9cdb0ffec172b
kingsley <knwachukwu73@gmail.com>
2025-04-20 13:07:04 +0100

    my work shoul be done

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,4 @@
 const express = require("express");
-const { Order } = require("../models/order");
-const { OrderItem } = require("../models/order-item");
-
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
@@ -10,58 +7,6 @@ const cors = require("cors");
 const dotenv = require('dotenv');
 dotenv.config()
 require("dotenv/config");
-
-app.post('/webhook',express.raw({type:'application/json'}), (req, res)=>{
-  const sig=req.headers['stripe-signature'];
-
-  let event;
-
-  try{
-    event = Stripe.webhook.constructEvent(req.body, sig, process.env.STRIPE_WEB_SECRET);
-  }catch (err){
-    console.error('webhook signature error:', err.message);
-    return res.status(400).send('webhook Error: ${err.message}');
-  }
-
-    if(event.type==='checkout.session.completed'){
-      const session=event.data.object;
-      const address=JSON.parse(session.metadata.address);
-      const cartItems=JSON.parse(session.metadata.items);
-
-       Promise.all(cartItems.map( item=>{
-        const newOrderItem = OrderItem({
-          quantity: item.quantity,
-          prodiuct:item.product._id
-        });
-        return newOrderItem.save();
-      }))
-      .then(orderItems=>{
-        const orderItemsIds = orderItems.map(item=>item._id);
-
-      const order = new Order({
-        orderItems:orderItemsIds,
-        shippingAddress1:address.address1,
-        shippingAddress2:address.address2 || '',
-        city:address.city,
-        zip:address.zip,
-        country:address.country,
-        phone:address.phone,
-        user:address.userId,
-        totalPrice:session.amount_total/100,
-        status:'pending',
-      });
-      return order.save();
-
-      })
-  .then(()=>res.status(200).send('Order created successfully'))
-  .catch(err=>{
-    console.error('Order creation failed:',err);
-    res.status(500).send('failed to save order');
-  });
-}else{
-  res.status(200).send('webhook recieved');
-}
-});
 
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
